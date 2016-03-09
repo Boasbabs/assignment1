7a6eca2ac41663e720aafd95b027bb096ab9bda3
Boasbabs <babalolasimeon@gmail.com>
2016-03-09 15:43:08 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,96 +1,83 @@
 var helloWorld = function() {
-	alert("Hello World!");
-};
-//helloWorld();
+      alert("Hello World!");
+      };
 
-var greeting = function() {
-	var firstName = prompt("please, first name?");
-	var checkFirst = parseInt(firstName);
-	var lastName = prompt("and your last name?");
-	var checkLast = parseInt(lastName);
- 	if(isNaN(checkFirst))
-  		{
-   			if(isNaN(checkLast)) {
-    		 alert("Hello, nice to met you  " + firstName.toUpperCase() + " " +lastName.toUpperCase());
-     		}
-     		else
-   				{alert("Nice effort but incorrect Last name!");}
-   		}
-   	else
-   		{alert("your first name is wrong! I don't trust you");}
-   }
-//greeting();
+        //greeting();
+        var greeting = function() {
+          var firstName = prompt("please, first name?");
+          var checkFirst = parseInt(firstName);
+          var lastName = prompt("and your last name?");
+          var checkLast = parseInt(lastName);
+          if(isNaN(checkFirst))
+              {
+                if(isNaN(checkLast)) {
+                 alert("Hello, nice to met you  " + firstName.toUpperCase() + " " +lastName.toUpperCase());
+                }
+                else
+                  {alert("Nice effort but incorrect Last name!");}
+              }
+            else
+              {alert("your first name is wrong! I don't trust you");}
+           }
 
-var greetPeople = function () {
-	var name = prompt('What is your name?');
-	if(name=='Alice') //If the name is 'Joe'
-		{ alert('Wow, cool - nice to met you Alice!');}
-	else if(name=='Bob') //If the name is 'Fred'
-		{ alert('Bob is a pretty generic name...');}
-	else
-		{ alert('I have no opinion on your name.');}
-};
-//greetPeople();
+      //greetPeople();
+      var greetPeople = function () {
+        var name = prompt('What is your name?');
+        if(name=='Alice') //If the name is 'Joe'
+          { alert('Wow, cool - nice to met you Alice!');}
+        else if(name=='Bob') //If the name is 'Fred'
+          { alert('Bob is a pretty generic name...');}
+        else
+          { alert('I have no opinion on your name.');}
+      };
 
-var sum = function() {
-  var number = parseInt(prompt("Number to add up"));
-  var count =0;
-  for(var i = 1; i < number +1; i++) {
-    count += i; 
-  }
-alert("It added up to  " + count);
-document.getElementById("result").innerHTML= count;
-};
-//sum();
+        //sum();
+        var sum = function() {
+          var number = parseInt(prompt("Number to add up"));
+          var count =0;
+          for(var i = 1; i < number +1; i++) {
+            count += i; 
+          }
+        alert("It added up to  " + count);
+        document.getElementById("result").innerHTML= count;
+        };
 
-var specialSum = function() {
-  var number = parseInt(prompt("Enter Number for special Sum"));
-  var count =0;
-  for(var i = 1; i < number +1; i++) {
-    if((i % 3 !== 0 && i % 5 === 0) || (i % 3 === 0 && i % 5 !== 0))  {
-      count += i;
-    }
-  }
-console.log(count);
-alert("It added up to  " + count);
-document.getElementById("result").innerHTML= count;
-};
+      //specialSum();
+        var specialSum = function() {
+          var number = parseInt(prompt("Enter Number for special Sum"));
+          var count =0;
+          for(var i = 1; i < number +1; i++) {
+            if((i % 3 !== 0 && i % 5 === 0) || (i % 3 === 0 && i % 5 !== 0))  {
+              count += i;
+            }
+          }
+        console.log(count);
+        alert("It added up to  " + count);
+        document.getElementById("result").innerHTML= count;
+        };
 
-//specialSum();
 
-// var multiTable = function() {
-//  	var n = parseInt(prompt("Multiplication Table of: "));
-//   	var counter=1;
-//   		for(var i=1; i < 13; i++) {
-//     	var result = i * n;
-//     console.log(n + " x " + i + " = " + result);
-//   }
-   
-// };
-// multiTable();
-var multiTable = function() {
-  var n = parseInt(prompt("Multiplication Table of: "));
-  var result = "";
-  for(var i=1; i < 13; i++) {
-    result += n + " x " + i + " = " + i*n + "<br/>";
-  }
-   document.getElementById("result").innerHTML= result;
-};
-//multiTable();
+      //mutliTable()
+      var multiTable = function() {
+        var n = parseInt(prompt("Multiplication Table of: "));
+        var result = "";
+        for(var i=1; i < 13; i++) {
+          result += n + " x " + i + " = " + i*n + "<br/>";
+        }
+         document.getElementById("result").innerHTML= result;
+      };
 
-function leapYear() {
-  var year = parseInt(prompt("Next 100 leap Years from: "));
-  var num = 100, lpYear = "";
-  while(num > 0) {
-    if(year % 4 === 0 && year % 100 != 0 || (year % 400 === 0)) {
-        lpYear += year + ", ";
-        year++;
-    	num--; } 
-      else 
-       		{ year++;}
-
-  }
-  document.getElementById("result").innerHTML = lpYear;
-  //console.log(lpYear);
-}
-//leapYear();
+      //leapYear()
+        function leapYear() {
+          var year = parseInt(prompt("Next 100 leap Years from: "));
+          var num = 100, lpYear = "";
+          while(num > 0) {
+            if(year % 4 === 0 && (year % 100 !== 0 || (year % 100 ==0 && year % 400 === 0))) {
+                lpYear += year + ", ";
+                
+              num-- } ;
+             // else { year++; }
+             year += 1;
+          }
+          document.getElementById("result").innerHTML = lpYear;
+        }
